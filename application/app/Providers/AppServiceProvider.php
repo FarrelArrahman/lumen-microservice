d0df88ad173a028dc5d37458ac45f6adf93f643d
Fabrizio Cafolla <fabrizio.cafolla97@gmail.com>
2018-12-24 12:49:50 +0100

    fixed configuration and service provider

--- application/app/Providers/AppServiceProvider.php
+++ application/app/Providers/AppServiceProvider.php
@@ -13,62 +13,6 @@
 		 */
 		public function register()
 		{
-			$this->setupConfig();
-			$this->registerAlias();
-			$this->registerSystem();
-			$this->registerServices();
-			$this->registerMiddleware();
-			$this->registerProviders();
+			//
 		}
-
-		/**
-		 * Register system providers Kernel/Console/Filesystem etc..
-		 */
-		protected function registerSystem() {}
-
-		/**
-		 * Register Services
-		 */
-		protected function registerServices() {}
-
-		/**
-		 * Register middleware
-		 */
-		protected function registerMiddleware()
-		{
-			$middlewares = config('providers.middlewares') ?: [];
-			foreach ($middlewares as $middleware) {
-				$this->app->middleware([
-					$middleware
-				]);
-			}
-
-			$route_middlewares = config('providers.route_middlewares') ?: [];
-			foreach ($route_middlewares as $key => $value) {
-				$this->app->routeMiddleware([
-					$key => $value
-				]);
-			}
-		}
-
-		/**
-		 * Register providers dependency
-		 */
-		protected function registerProviders() {}
-
-		/**
-		 * Load alias
-		 */
-		protected function registerAlias()
-		{
-			$aliases = config('providers.alias') ?: [];
-			foreach ($aliases as $key => $value) {
-				class_alias($value, $key);
-			}
-		}
-
-		/**
-		 * Load config
-		 */
-		protected function setupConfig() {}
 	}
--- application/config/providers.php
+++ application/config/providers.php
@@ -1,8 +1,19 @@
 <?php
 	return [
 		/*
-		 * Providers always load in Core
-		 */
+		|--------------------------------------------------------------------------
+		| Providers
+		|--------------------------------------------------------------------------
+		|
+		| Don't forget to set this env file.
+		| You can add providers to load in app
+		|
+		| Global: register the key service providers for the app
+		| Production: register service providers only if the development environment is production
+		| Staging: register service providers only if the development environment is staging
+		| Develop: register service providers only if the development environment is develop
+		|
+		*/
 		'global' => [
 			'core' => \Core\Providers\CoreServiceProvider::class,
 			'auth' => \Core\Providers\AuthServiceProvider::class,
@@ -10,20 +21,27 @@
 			'app' => App\Providers\AppServiceProvider::class,
 		],
 
-		/*
-		 * Providers load when env is production
-		 */
 		'production' => [
 			//
 		],
 
-		/*
-		 * Providers load when env is local
-		 */
+		'staging' => [
+			//
+		],
+
 		'local' => [
 			//
 		],
 
+		/*
+		|--------------------------------------------------------------------------
+		| Load Alias and Middleware
+		|--------------------------------------------------------------------------
+		|
+		| Alias: create alias of class ('aliasName' => PathClass::class)
+		| Middlewares: register middleware for all application routes (PathClass::class)
+		| Route middlewares: register middleware in the application that can then be used to protect routes or/and controllers ( 'middlewareName' => PathClass::class)
+		*/
 		'alias' => [],
 
 		'middlewares' => [
