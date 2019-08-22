20f4492805810c3ee70d0936de566dda8b9061c5
FabrizioCafolla <developer@fabriziocafolla.com>
2019-08-22 13:31:03 +0200

    remove: framework package and config file add: permission into Dockerfile backend

--- application/backend/bootstrap/app.php
+++ application/backend/bootstrap/app.php
@@ -1,63 +1,81 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+	require_once __DIR__ . '/../vendor/autoload.php';
 
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
+	(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(dirname(__DIR__)))->bootstrap();
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| Here we will load the environment and create the application instance
-| that serves as the central piece of this framework. We'll use this
-| application as an "IoC" container and router for this framework.
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| Create The Application
+	|--------------------------------------------------------------------------
+	|
+	| Here we will load the environment and create the application instance
+	| that serves as the central piece of this framework. We'll use this
+	| application as an "IoC" container and router for this framework.
+	|
+	*/
 
-$app = new Laravel\Lumen\Application(
-    realpath(__DIR__ . '/application/')
-);
+	$app = new Laravel\Lumen\Application(realpath(__DIR__ . '/application/'));
 
-$app->instance('path.config', app()->basePath() . DIRECTORY_SEPARATOR . 'config');
-$app->instance('path.storage', app()->basePath() . DIRECTORY_SEPARATOR . 'storage');
+	$app->instance('path.config', app()->basePath() . DIRECTORY_SEPARATOR . 'config');
+	$app->instance('path.storage', app()->basePath() . DIRECTORY_SEPARATOR . 'storage');
 
-$app->withFacades();
-$app->withEloquent();
+	$app->withFacades();
+	$app->withEloquent();
 
-/*
-|--------------------------------------------------------------------------
-| Manager Service Provider
-|--------------------------------------------------------------------------
-|
-| The automatic providers manager loads the providers included in config/manager.php file
-| If you want to load new providers just enter in the array(global, local, production)
-| 'name'  => \path\NameProvider::class
-| This will allow you to better manage the loading.
-| If you want to create a provider through artisan, just run:
-| php artisan create:provider
-|
-*/
+	$app->configure('filesystems');
+	$app->configure('api');
 
-//Load Manager Providers of Framework
-$app->register(Kosmosx\Framework\Providers\ManagerServiceProvider::class);
+	$app->singleton('filesystem', function ($app) {
+		return $app->loadComponent('filesystems', Illuminate\Filesystem\FilesystemServiceProvider::class, 'filesystem');
+	});
 
-/*
-|--------------------------------------------------------------------------
-| Load The Application Routes
-|--------------------------------------------------------------------------
-|
-| Next we will include the routes file so that they can all be added to
-| the application. This will provide all of the URLs the application
-| can respond to, as well as the controllers that may handle them.
-|
-*/
-$app->router->group([
-    'namespace' => 'App\Http\Controller',
-], function ($router) {
-    require __DIR__ . '/../routes/web.php';
-});
+	$app->singleton(Illuminate\Contracts\Debug\ExceptionHandler::class, Kosmosx\Response\Laravel\Exceptions\LumenHandler::class);
 
-return $app;
+	$app->singleton(Illuminate\Contracts\Console\Kernel::class, App\Console\Kernel::class);
+
+	/*
+	|--------------------------------------------------------------------------
+	| Manager Service Provider
+	|--------------------------------------------------------------------------
+	|
+	| The automatic providers manager loads the providers included in config/manager.php file
+	| If you want to load new providers just enter in the array(global, local, production)
+	| 'name'  => \path\NameProvider::class
+	| This will allow you to better manage the loading.
+	| If you want to create a provider through artisan, just run:
+	| php artisan create:provider
+	|
+	*/
+
+	$app->register(Kosmosx\Helpers\HelpersServiceProvider::class);
+	$app->register(Kosmosx\Auth\AuthServiceProvider::class);
+	$app->register(Kosmosx\Response\Laravel\Providers\ResponseServiceProvider::class);
+	$app->register(Kosmosx\Cache\CacheServiceProvider::class);
+	$app->register(Kosmosx\Support\SupportServiceProvider::class);
+
+	$app->register(App\Providers\AppServiceProvider::class);
+
+	$app->routeMiddleware(array());
+
+	$app->middleware(array(
+		Kosmosx\Support\Middleware\CorsMiddleware::class
+	));
+
+	/*
+	|--------------------------------------------------------------------------
+	| Load The Application Routes
+	|--------------------------------------------------------------------------
+	|
+	| Next we will include the routes file so that they can all be added to
+	| the application. This will provide all of the URLs the application
+	| can respond to, as well as the controllers that may handle them.
+	|
+	*/
+	$app->router->group([
+		'namespace' => 'App\Http\Controller',
+	], function ($router) {
+		require __DIR__ . '/../routes/web.php';
+	});
+
+	return $app;
